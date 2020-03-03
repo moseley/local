c2b230c4eea0bc4ad3bce798f9c85d90c28ef221
moseley <jeremymoseley@me.com>
2020-03-03 08:52:51 -0800

    Add travis

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+//import { render } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import Header from './components/Header';
+
+configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  it("doesn't crash", () => {
+    const wrapper = document.createElement('div');
+    ReactDOM.render(
+      <Router>
+        <App />
+      </Router>,
+      wrapper
+    );
+    ReactDOM.unmountComponentAtNode(wrapper);
+  });
+
+  // it('contains <Header />', () => {
+  //   const wrapper = shallow(<App />);
+  //   expect(wrapper.find(Header)).toHaveLength(1);
+  // });
+
+  // it('contains <Switch />', () => {
+  //   const wrapper = shallow(<App />);
+  //   expect(wrapper.find(Switch)).toHaveLength(1);
+  // });
 });
