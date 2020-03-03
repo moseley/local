384ef3808421036b26bfc30c32212cae93c37bac
moseley <jeremymoseley@me.com>
2020-03-03 11:19:12 -0800

    Static width

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -10,7 +10,7 @@ import Typography from '@material-ui/core/Typography';
 const useStyles = makeStyles(theme => ({
   root: {
     '& > *': {
-      maxWidth: 345
+      width: 345
     }
   },
   media: {
