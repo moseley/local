aa4dfe7de026c7e94bee44959ed54c7e23166047
moseley <jeremymoseley@me.com>
2020-03-18 16:20:57 -0700

    Mock tertiary store with provider for test

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 import App from './App';
 
 describe('<App />', () => {
+  const initialState = { cats: { tertiary: '' } };
+  const middlewares = [];
+  const mockStore = configureStore(middlewares);
+  const store = mockStore(initialState);
+
   it("doesn't crash", () => {
     const wrapper = document.createElement('div');
     ReactDOM.render(
-      <Router>
-        <App />
-      </Router>,
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>,
       wrapper
     );
     ReactDOM.unmountComponentAtNode(wrapper);
