ffaaee23e5ae47de0ffff4b204ce29ca2eb3dfeb
moseley <jeremymoseley@me.com>
2020-03-18 16:06:30 -0700

    App test fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-//import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
-import Header from './components/Header';
-
-configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it("doesn't crash", () => {
@@ -21,14 +15,4 @@ describe('<App />', () => {
     );
     ReactDOM.unmountComponentAtNode(wrapper);
   });
-
-  // it('contains <Header />', () => {
-  //   const wrapper = shallow(<App />);
-  //   expect(wrapper.find(Header)).toHaveLength(1);
-  // });
-
-  // it('contains <Switch />', () => {
-  //   const wrapper = shallow(<App />);
-  //   expect(wrapper.find(Switch)).toHaveLength(1);
-  // });
 });
